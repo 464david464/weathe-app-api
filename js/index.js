a988f346e9e19523a2f22a8bacc0fda0d4263fc6
david weinstein <davidmenachem464@gmail.com>
2022-03-04 15:43:58 +0200

    v

--- js/index.js
+++ js/index.js
@@ -1,51 +1,59 @@
+let d = new Date();
+console.log(d.getTime());
+let renderDTIme = document.querySelector(".datd");
+
 let myHeaders = new Headers();
 
-  let requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
+let requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
 
 fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Mevo+Betar&lang=he&units=metric&appid=9ac9607796efff4f0762a85bc43efa57",
-    requestOptions
+  "https://api.openweathermap.org/data/2.5/weather?q=חרקוב&lang=he&units=metric&appid=9ac9607796efff4f0762a85bc43efa57",
+  requestOptions
+)
+  .then((response) => response.json())
+  .then((result) =>
+    getWeatherDetails(
+      parseInt(result.main.temp) + "°",
+      result.name,
+      result.weather[0].icon,
+      result.weather[0].description,
+      result.main.feels_like
+    )
   )
-    .then((response) => response.json())
-    .then((result) => getWeatherDetails(parseInt(result.main.temp) + '°', result.name, result.weather[0].icon, result.weather[0].description))
-    .catch((error) => console.log("error", error));
+  .catch((error) => console.log("error", error));
 
-    function getWeatherDetails(temp, city, icon, escription) {
-        let ststusImg = document.querySelector('img');
-        let statusOfWeather = document.querySelector('.p-status');
-        let area = document.querySelector('.area');
-        let deg = document.querySelector('.deg');
-        let about = document.querySelector('.feel-like');
+function getWeatherDetails(temp, city, icon, escription, feelsLike) {
+  let ststusImg = document.querySelector("img");
+  let statusOfWeather = document.querySelector(".p-status");
+  let area = document.querySelector(".area");
+  let deg = document.querySelector(".deg");
+  let about = document.querySelector(".feel-like");
+  let aboutText = document.querySelector(".feel-like-text");
 
-        deg.innerText = temp;
-        area.innerText = city
-        if(area.textContent == 'מבוא ניתר') {
-            area.innerText = 'מבוא ביתר'
-        }
-        ststusImg.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
-        statusOfWeather.innerText = escription;
-    }
+  deg.innerText = temp;
+  area.innerText = city;
+  ststusImg.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  statusOfWeather.innerText = escription;
+  about.innerText = parseInt(feelsLike) + '°';
+  aboutText.innerText = 'מרגיש כמו'
 
-    
-        let myListHeaders = new Headers();
-
-  let requestOptionsList = {
-    method: "GET",
-    headers: myListHeaders,
-    redirect: "follow",
-  };
-
-fetch(
-    "../json/city.list.json",
-    requestOptionsList
-  )
-    .then((response) => response.json())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-
-    function sityList(nameOfTheCity) {    
 }
+
+let myListHeaders = new Headers();
+
+let requestOptionsList = {
+  method: "GET",
+  headers: myListHeaders,
+  redirect: "follow",
+};
+
+fetch("../json/city.list.json", requestOptionsList)
+  .then((response) => response.json())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
+
+function sityList(nameOfTheCity) {}
